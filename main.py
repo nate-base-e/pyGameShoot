8ed0ead8a02783adb888fc994535d210a74eccef
nnissen <nnissen@grandview.edu>
2023-11-15 14:50:56 -0600

    Merge remote-tracking branch 'origin/master'

--- main.py
+++ main.py
@@ -1,74 +1,78 @@
-import pygame
+# import pygame
+#
+# # pygame setup
+# pygame.init()
+# screen = pygame.display.set_mode((1280, 720))
+# clock = pygame.time.Clock()
+# running = True
+# dt = 0
+#
+# player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+#
+#
+# class Character(pygame.sprite.Sprite):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.image = pygame.image.load("assets/rocket_ship.jpg")
+#         self.rect = self.image.get_rect()
+#
+#         self.x = 500
+#         self.y = 500
+#
+#     def update(self):
+#         # Update the character's position
+#
+#         self.x += 1
+#         self.y += 1
+#
+#     def draw(self, screen):
+#         # Draw the character to the screen
+#
+#         screen.blit(self.image, (self.x, self.y))
+#
+#
+# # Create a Sprite group
+# sprites = pygame.sprite.Group()
+#
+# # Create a character object
+# character = Character()
+#
+# # Add the character to the Sprite group
+# sprites.add(character)
+# bGimage = pygame.image.load("assets/galaxy.jpg")
+# while running:
+#     # poll for events
+#     # pygame.QUIT event means the user clicked X to close your window
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running = False
+#
+#
+#     background_image = pygame.transform.scale(bGimage, screen.get_size())
+#     screen.blit(background_image, (0, 0))
+#
+#     # this gets the keys that were pressed
+#     keys = pygame.key.get_pressed()
+#     if keys[pygame.K_w]:
+#         character.y -= 300 * dt
+#     if keys[pygame.K_s]:
+#         character.y += 300 * dt
+#     if keys[pygame.K_a]:
+#         character.x -= 300 * dt
+#     if keys[pygame.K_d]:
+#         character.x += 300 * dt
+#
+#     character.draw(screen)
+#     # flip() the display to put your work on screen
+#     pygame.display.flip()
+#
+#     # limits FPS to 60
+#     # dt is delta time in seconds since last frame, used for framerate-
+#     # independent physics.
+#     dt = clock.tick(60) / 1000
+#
+# pygame.quit()
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
-dt = 0
-
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 
-class Character(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-
-        self.image = pygame.image.load("assets/rocket_ship.jpg")
-        self.rect = self.image.get_rect()
-
-        self.x = 500
-        self.y = 500
-
-    def update(self):
-        # Update the character's position
-
-        self.x += 1
-        self.y += 1
-
-    def draw(self, screen):
-        # Draw the character to the screen
-
-        screen.blit(self.image, (self.x, self.y))
-
-
-# Create a Sprite group
-sprites = pygame.sprite.Group()
-
-# Create a character object
-character = Character()
-
-# Add the character to the Sprite group
-sprites.add(character)
-
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    bGimage = pygame.image.load("assets/galaxy.jpg")
-    background_image = pygame.transform.scale(bGimage, screen.get_size())
-    screen.blit(background_image, (0, 0))
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        character.y -= 300 * dt
-    if keys[pygame.K_s]:
-        character.y += 300 * dt
-    if keys[pygame.K_a]:
-        character.x -= 300 * dt
-    if keys[pygame.K_d]:
-        character.x += 300 * dt
-
-    character.draw(screen)
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
-
-pygame.quit()
---
+++ selection box.py
@@ -0,0 +1,87 @@
+import pygame
+import random
+
+# Initialize pygame
+pygame.init()
+
+# Define colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Set up the display
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Pygame Rectangle with Font")
+
+# Define the font
+font = pygame.font.Font(None, 32)
+
+# Create the rectangle
+rectangle = pygame.Rect(100, 100, 200, 100)
+color = WHITE
+
+# Create the lines of font
+text1 = font.render("Line 1", True, BLACK)
+text2 = font.render("Line 2", True, BLACK)
+text3 = font.render("Line 3", True, BLACK)
+
+# Create the selection arrow
+arrow_pos = (rectangle.left + rectangle.width - 30, rectangle.top + 30)
+arrow_color = RED
+
+# Define the selection
+selection = 0
+
+# Main loop
+running = True
+while running:
+    # Check for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        # Check for mouse clicks
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+
+            # Check if the mouse clicked inside the rectangle
+            if rectangle.collidepoint(mouse_pos):
+                # Check which line of font was clicked
+                if text1.get_rect(center=(rectangle.centerx,rectangle.centery-32)).collidepoint(mouse_pos):
+                    selection = 0
+                elif text2.get_rect(center=(rectangle.centerx+0, rectangle.centery)).collidepoint(mouse_pos):
+                    selection = 1
+                elif text3.get_rect(center=(rectangle.centerx+0, rectangle.centery+32)).collidepoint(mouse_pos):
+                    selection = 2
+
+    # Fill the background
+    screen.fill(BLACK)
+
+    # Draw the rectangle
+    pygame.draw.rect(screen, color, rectangle)
+
+    # Draw the lines of font
+    screen.blit(text1, text1.get_rect(center=(rectangle.centerx,rectangle.centery-32)))
+    screen.blit(text2, text2.get_rect(center=(rectangle.centerx,rectangle.centery)))
+    screen.blit(text3, text3.get_rect(center=(rectangle.centerx,rectangle.centery+32)))
+
+    # Draw the selection arrow
+    if selection == 0:
+        arrow_pos = (rectangle.left + rectangle.width - 30, rectangle.top + 15)
+        arrow_color = RED
+    elif selection == 1:
+        arrow_pos = (rectangle.left + rectangle.width - 30, rectangle.top + 45)
+        arrow_color = GREEN
+    elif selection == 2:
+        arrow_pos = (rectangle.left + rectangle.width - 30, rectangle.top + 80)
+        arrow_color = BLUE
+
+    pygame.draw.polygon(screen, arrow_color, [(arrow_pos[0], arrow_pos[1]), (arrow_pos[0] + 10, arrow_pos[1] + 5), (arrow_pos[0] + 10, arrow_pos[1] - 5)])
+
+    # Update the display
+    pygame.display.flip()
+
+# Quit pygame
+pygame.quit()
